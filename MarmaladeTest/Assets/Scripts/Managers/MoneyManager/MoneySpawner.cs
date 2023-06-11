ca1ed57734c40594f9b682355b678995bee11b70
Anish Pithakote <muttsang@gmail.com>
2023-06-11 01:34:35 +0100

    Made a basic money spawner

--- MarmaladeTest/Assets/Scripts/Managers/MoneyManager/MoneySpawner.cs
+++ MarmaladeTest/Assets/Scripts/Managers/MoneyManager/MoneySpawner.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 public class MoneySpawner : MonoBehaviour
 {
     [SerializeField] private List<MoneySO> _moneyTypes = new List<MoneySO>();
+    [SerializeField] private Transform _spawnArea;
 
+    private Vector3 _spawnPosition;
+    [SerializeField] private Vector3 _spawnSpan;
+    [SerializeField] private float _spawnRadius;
+    [SerializeField] private float _spawnIntervalsInSeconds = 2;
+    [SerializeField] private GameObject _spawnPrefab;
     protected virtual void Start()
     {
         SpawnMoney();
@@ -13,9 +19,15 @@ public class MoneySpawner : MonoBehaviour
 
     private async void SpawnMoney()
     {
-        await SpawnInInterval(2000);
+        await SpawnInInterval((int)(_spawnIntervalsInSeconds * 1000));
         Debug.Log("Spawning Money");
 
+        _spawnPosition = transform.position +
+                                    new Vector3(Random.Range(-Random.insideUnitSphere.x * _spawnRadius, Random.insideUnitSphere.x * _spawnRadius),
+                                                            0.0f,
+                                                            Random.Range(-Random.insideUnitSphere.z * _spawnRadius, Random.insideUnitSphere.z * _spawnRadius));
+
+        Instantiate(_spawnPrefab, _spawnPosition, Quaternion.identity);
         SpawnMoney();
     }
     private async Task SpawnInInterval(int time)
@@ -24,5 +36,11 @@ public class MoneySpawner : MonoBehaviour
        await Task.Delay(time);
 
         
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, _spawnRadius);
     }
 }
