683292231765edf86ed16cdffc6888407c2774a7
Anish Pithakote <muttsang@gmail.com>
2023-06-11 15:43:12 +0100

    Added AI car prefab and made some collision layers settings

---
+++ MarmaladeTest/Assets/Scripts/Cars/AICar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICar : Cars
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    public override void ResetPosition()
+    {
+        base.ResetPosition();   
+    }
+}
